fd6c4d370d76c4f0364145c1d6ab353cb540b4db
QuentinHerszko <quentin.herszkowicz@outlook.fr>
2022-06-15 11:49:35 +0200

    Modification du calcul du vecteur gamma
    Reste à faire en sorte que l'interpolation  coîncide avec les mu_tilde précédents -> normalisation ?
    
    Modifications approtées par Quentin Herszkowicz

--- ppi.m
+++ ppi.m
@@ -1,6 +1,6 @@
-function [next_mu_tilde,next_fmu_tilde,i] = ppi(gamma,sig,mu_tilde,mu,fmu,k)
-  Imu = rbf_val(gamma,sig,mu_tilde,mu,k);
+function [next_mu_tilde,next_fmu_tilde,next_Imu_tilde,i] = ppi(mu,fmu,Imu)
   [m,i] = max(abs(fmu - Imu));
   next_mu_tilde = mu(i);
   next_fmu_tilde = fmu(i);
+  next_Imu_tilde = Imu(i);
 endfunction

986923d64b29085f66e1309f17ef7951b02b98a6
QuentinHerszko <quentin.herszkowicz@outlook.fr>
2022-07-18 09:42:24 +0200

    Correction recherche de mu_tilde

--- rbf_coef.m
+++ rbf_coef.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function [gamma, sig, mu_tilde,k] = rbf_coef(mu,fmu,x,y)
   % informations de base
   [M,d] = size(mu);
@@ -28,7 +27,7 @@ function [gamma, sig, mu_tilde,k] = rbf_coef(mu,fmu,x,y)
     plot(mu,g,'*k','linewidth',2)
 
     % - point d'interpolation à traiter :
-    [m1,i] = max(g);
+    [m1,i] = max(abs(g));
     
     % - enregistrement de la donnée :
     mu_tilde(k) = mu(i);
@@ -76,45 +75,11 @@ function [gamma, sig, mu_tilde,k] = rbf_coef(mu,fmu,x,y)
 
   endwhile
 
-err_approx
-figure()
-grid on
-plot(1:10,err_approx,'*b','linewidth',2)
-xlabel("Nombre de points d'interpolation traité")
-ylabel("Erreur d'approximation")
-set(gca,'fontsize',15)
+  figure(12)
+  grid on
+  plot(1:10,err_approx,'*b','linewidth',2)
+  xlabel("Nombre de points d'interpolation traité")
+  ylabel("Erreur d'approximation")
+  set(gca,'fontsize',15)
 
 endfunction
-=======
-function [gamma, sig, mu_tilde,k] = rbf_coef(mu,fmu)
-  [M,d] = size(mu);
-  gamma = zeros(M,1);
-  sig = zeros(1,M);
-  mu_tilde = zeros(M,d);
-  fmu_tilde = zeros(M,1);
-  Imu = zeros(M,1);
-  Imu_tilde = zeros(M,1);
-  seuil = 1e-2;
-  test = seuil + 1;
-  k = 1;
-  
-  while test > seuil && k <= M
-
-    % - Mise à jour de mu_tilde
-    [mu_tilde(k),fmu_tilde(k),Imu_tilde(k),i] = ppi(mu,fmu,Imu);
-
-    % - Mise à jour de gamma que sur le point d'interpolation en question
-    gamma(k) = fmu_tilde(k) - Imu_tilde(k);
-
-    % - Mise à jour de sigma en fonction des résultats précédents
-    [mup,fmup] = recherche_mup(mu,fmu,i);
-    sig(k) = recherche_sig_opt(sig,gamma,mu_tilde,fmu_tilde,mup,fmup,k);
-
-    % - Test
-    Imu = rbf_val(gamma,sig,mu_tilde,mu,k);
-    test = max(abs(fmu-Imu));
-
-    k = k + 1;
-  endwhile
-endfunction
->>>>>>> ae169162acb5089465de6d364cbc28a27ec5c3ea

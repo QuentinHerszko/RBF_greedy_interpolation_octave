cd1c86340c8ff11547053df27db6ab8d99d7f2ff
QuentinHerszko <quentin.herszkowicz@outlook.fr>
2022-07-25 15:18:00 +0200

    Correction de bug

--- ppi.m
+++ ppi.m
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-function [next_mu_tilde,next_fmu_tilde,i] = ppi(mu,fmu,Imu)
-=======
 function [next_mu_tilde,next_fmu_tilde,next_Imu_tilde,i] = ppi(mu,fmu,Imu)
->>>>>>> ae169162acb5089465de6d364cbc28a27ec5c3ea
   [m,i] = max(abs(fmu - Imu));
   next_mu_tilde = mu(i);
   next_fmu_tilde = fmu(i);
